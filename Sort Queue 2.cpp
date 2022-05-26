d52b6c6b8fba0bdefe93bfbacd823d683c48c4b1
Youssef-Nassar <89210206+Youssef-Nassar@users.noreply.github.com>
2022-05-26 08:53:01 +0200

    Update Sort Queue 2.cpp

--- Sort Queue 2.cpp
+++ Sort Queue 2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <queue>
+#include<string>
+using namespace std;
+  
+void showpq(priority_queue<int, vector<int>, greater<int> > gq){
+    priority_queue<int, vector<int>, greater<int> > g = gq;
+    while (!g.empty()) {
+        cout<< char(g.top());
+        g.pop();
+    }
+}
+  
 
+int main()
+{
+    priority_queue<int, vector<int>, greater<int> > q;
+    string s = "zhYauc@MapaOwp!";
+    const int SIZE = s.length();
+    
+
+    for(int i=0; i<SIZE; i++)
+        q.push(toupper(int(s[i])));
+        
+    showpq(q);   
+    
+  
+    return 0;
+}
