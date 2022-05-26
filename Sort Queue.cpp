4e992e9f6aa766082ccf9afab570a3982fca1ede
Youssef-Nassar <89210206+Youssef-Nassar@users.noreply.github.com>
2022-05-26 08:32:14 +0200

    Update Sort Queue.cpp

--- Sort Queue.cpp
+++ Sort Queue.cpp
@@ -1,118 +1,126 @@
 #include<iostream>
 #include<string>
+#include<bits/stdc++.h>
 using namespace std;
 
 struct Node
 {
-    char data;
-    Node *next;
+  char data;
+  Node *next;
 };
 
 
 //Queue
 class Queue
 {
-    private:
-         Node* front;
-         Node* rear; 
-         Node* temp;
-    public:
-        Queue();
-        void enQueue (char val); 
-        char dequeue (); 
-        void displayQueue();
-        char topDisplay ();
+private:
+  Node * front;
+  Node *rear;
+  Node *temp;
+public:
+    Queue ();
+  void enQueue (char val);
+  char dequeue ();
+  void displayQueue ();
+  char topDisplay ();
 };
-Queue :: Queue()
+Queue::Queue ()
 {
-    front = NULL;
-    rear = NULL;
-}
-void Queue ::  enQueue (char val) 
-{
-        if (rear == NULL) 
-        {
-            rear = new Node;
-            rear -> next = NULL;
-            rear -> data = val;
-            front = rear;
-        }
-        else 
-        {
-            temp = new Node;
-            rear -> next = temp;
-            temp -> data = val;
-            temp -> next = NULL;
-            rear = temp;
-        }
+  front = NULL;
+  rear = NULL;
 }
 
-void Queue :: displayQueue()
+void
+Queue::enQueue (char val)
 {
-        temp = front;
-        if ((front == NULL) && (rear == NULL))
-            cout<<"Queue is empty"<<endl;
-        else 
-        {
-            while (temp != NULL) 
-            {
-                cout<<temp -> data << " ";
-                temp = temp -> next;
-            }
-            cout<<endl;
-        }
+  if (rear == NULL)
+    {
+      rear = new Node;
+      rear->next = NULL;
+      rear->data = val;
+      front = rear;
     }
-char Queue :: dequeue ()
+  else
+    {
+      temp = new Node;
+      rear->next = temp;
+      temp->data = val;
+      temp->next = NULL;
+      rear = temp;
+    }
+}
+
+void
+Queue::displayQueue ()
 {
-        temp = front;
-        char deletedData;
-        if (front == NULL)
-        cout<<"Queue is empty!!" << endl;
-        else if (temp->next != NULL) 
-        {
-            temp = temp->next;
-            deletedData = front -> data;
-            delete (front);
-            front = temp;
-        }
-        else 
-        {
-            deletedData = front->data;
-            delete (front);
-            front = NULL;
-            rear = NULL;
-        }
-        return deletedData;
+  temp = front;
+  if ((front == NULL) && (rear == NULL))
+    cout << "Queue is empty" << endl;
+  else
+    {
+      while (temp != NULL)
+	{
+	  cout << temp->data << " ";
+	  temp = temp->next;
+	}
+      cout << endl;
     }
-    
-char Queue :: topDisplay ()
+}
+
+char
+Queue::dequeue ()
 {
-        temp = front;
-        char topData == NULL;
-        if (front == NULL)
-        cout<<"Queue is empty!!" << endl;
-        else 
-            topData = front -> data;
-        return topData;
+  temp = front;
+  char deletedData;
+  if (front == NULL)
+    cout << "Queue is empty!!" << endl;
+  else if (temp->next != NULL)
+    {
+      temp = temp->next;
+      deletedData = front->data;
+      delete (front);
+      front = temp;
     }
+  else
+    {
+      deletedData = front->data;
+      delete (front);
+      front = NULL;
+      rear = NULL;
+    }
+  return deletedData;
+}
+
+char
+Queue::topDisplay ()
+{
+  temp = front;
+  char topData;
+  if (front == NULL)
+    cout << "Queue is empty!!" << endl;
+  else
+    topData = front->data;
+  return topData;
+}
 
 
-int main()
-{
+
+
+int main (){
     Queue q;
     string s = "zhYauc@MapaOwp!";
-    int size = 0;
-    
-    for(int i = 0; i <= s.length; i++){
-        for(int j = 0; j <= size; j++){
-            if(q.topDisplay() == NULL)
-                q.dequeue(s[i]);
-            else if(){
-                
-            }        
-        }
+    const int SIZE = s.length();
+    string sUp;
+
+    for(int i=0; i<SIZE; i++)
+        sUp += toupper(s[i]);
         
-        
-    }
+    sort(sUp.begin(), sUp.end());
+
+    for (int j=0; j < SIZE; j++)
+        q.enQueue (toupper(sUp[j]));
     
+    
+    for (int k = 0; k < SIZE; k++)
+        cout << q.dequeue();
 }
